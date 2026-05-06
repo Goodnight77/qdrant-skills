19550fe2b2ba884936cd5e4eea76ab50a7956876
Andrey Vasnetsov <andrey@vasnetsov.com>
2026-05-06 19:59:32 +0200

    pre-release fix

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 rm -rf public
 cp -r skills public
 
+find public -name '*.md' -print0 | xargs -0 sed -i 's|https://search\.qdrant\.tech/md/|/md/|g'
+
 bash scripts/generate_sitemap.sh public
